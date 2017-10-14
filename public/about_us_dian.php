babb8a1f763e17ce66a36e3acd9c9c03fd93d564
Unknown <ddaiazardian@gmail.com>
2017-10-14 18:00:30 +0700

    add maps and add maps at about us

--- public/about_us_dian.php
+++ public/about_us_dian.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html class="no-js" lang="en">
+<div class="no-js" lang="en">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
@@ -31,7 +31,7 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
-<body>
+<div>
 
 <!-- LOADER -->
 <div id="loader">
@@ -186,188 +186,20 @@ include "header.php";
           <span class=" margin-top-20">The weather started getting rough - the tiny ship was tossed. If not for the courage of the fearless crew the Minnow would be lost. the Minnow always wanted to have a neighbor just like you. I've always wanted to live in a neighborhood with you. </span> </div>
       </div>
     </section>
-
-                <!-- Team -->
-    <section class="padding-top-100 padding-bottom-100">
-      <div class="container">
-        <!-- HEADING BLOCK -->
-        <div class="heading-block text-center margin-bottom-80">
-          <h3>Our Expert Team Members</h3>
-          <hr>
-          <span class=" margin-top-20">The weather started getting rough - the tiny ship was tossed. If not for the courage of the fearless crew the Minnow would be lost. the Minnow always wanted to have a neighbor just like you. I've always wanted to live in a neighborhood with you. </span> </div>
+          <section class="why-choose padding-top-100 padding-bottom-100">
+              <div class="container">
+                  <?php
+                  include "maps.php";
+                  ?>
+              </div>
+          </section>
       </div>
-
-        <!-- Team -->
-      <div class="expert-team">
-        <div class="container">
-          <ul class="row">
-            <!-- TEAM MEMBER -->
-            <li class="col-md-3">
-              <article class="z-depth-1 padding-30">
-                <div class="img-circle"> <img src="images/team-img-1.jpg" alt="" >
-                  <h5>James Hopper</h5>
-                  <span>Lead Developer</span>
-                  <p>We finally got a piece of the pie. Makin' your way in the world today week long.</p>
-                  <ul class="social-icons">
-                    <li><a href="#."><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="#."><i class="fa fa-twitter"></i></a></li>
-                    <li><a href="#."><i class="fa fa-google"></i></a></li>
-                  </ul>
-                </div>
-              </article>
-            </li>
-
-              <!-- TEAM MEMBER -->
-            <li class="col-md-3">
-              <article class="z-depth-1 padding-30">
-                <div class="img-circle"> <img src="images/team-img-1.jpg" alt="" >
-                  <h5>martin guptil</h5>
-                  <span>web Developer</span>
-                  <p>We finally got a piece of the pie. Makin' your way in the world today week long.</p>
-                  <ul class="social-icons">
-                    <li><a href="#."><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="#."><i class="fa fa-twitter"></i></a></li>
-                    <li><a href="#."><i class="fa fa-google"></i></a></li>
-                  </ul>
-                </div>
-              </article>
-            </li>
-
-              <!-- TEAM MEMBER -->
-            <li class="col-md-3">
-              <article class="z-depth-1 padding-30">
-                <div class="img-circle"> <img src="images/team-img-1.jpg" alt="" >
-                  <h5>andrew hilson</h5>
-                  <span>Designer</span>
-                  <p>We finally got a piece of the pie. Makin' your way in the world today week long.</p>
-                  <ul class="social-icons">
-                    <li><a href="#."><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="#."><i class="fa fa-twitter"></i></a></li>
-                    <li><a href="#."><i class="fa fa-google"></i></a></li>
-                  </ul>
-                </div>
-              </article>
-            </li>
-
-              <!-- TEAM MEMBER -->
-            <li class="col-md-3">
-              <article class="z-depth-1 padding-30">
-                <div class="img-circle"> <img src="images/team-img-1.jpg" alt="" >
-                  <h5>robert jakis</h5>
-                  <span>Marketing</span>
-                  <p>We finally got a piece of the pie. Makin' your way in the world today week long.</p>
-                  <ul class="social-icons">
-                    <li><a href="#."><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="#."><i class="fa fa-twitter"></i></a></li>
-                    <li><a href="#."><i class="fa fa-google"></i></a></li>
-                  </ul>
-                </div>
-              </article>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </section>
-
-                <!-- News Letter -->
-    <div class="container">
-      <section class="news-letter">
-        <div class="row">
-          <div class="col-md-6">
-            <h5>Subscribe to our newsletter</h5>
-            <p>You can unsubscribe anytime you want</p>
-          </div>
-          <div class="col-md-6">
-            <form>
-              <input type="email" class="z-depth-1" placeholder="Email address" required>
-              <button type="submit" class="btn waves-effect waves-light">subscribe</button>
-            </form>
-          </div>
-        </div>
-      </section>
     </div>
-        </div>
-        <!-- End Content -->
-
-        <!-- Footer -->
-
-        <footer>
-            <div class="container">
-                <div class="row">
-
-                    <!-- About -->
-                    <div class="col-md-3"> <img class="margin-bottom-20" src="images/logo-footer.png" alt="" >
-                        <p>Said Californ'y is the place you ought to be So they loaded up the truck and moved to Beverly. Hills that is.</p>
-
-                        <!-- Social Icon -->
-                        <ul class="social-icons">
-                            <li><a href="#."><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="#."><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="#."><i class="fa fa-google"></i></a></li>
-                            <li><a href="#."><i class="fa fa-linkedin"></i></a></li>
-                            <li><a href="#."><i class="fa fa-dribbble"></i></a></li>
-                        </ul>
-                    </div>
-
-                    <!-- Our Services -->
-                    <div class="col-md-3">
-                        <h5>Our Services</h5>
-                        <ul class="links">
-                            <li><a href="#.">Wed Design</a></li>
-                            <li><a href="#."> Wed Development</a></li>
-                            <li><a href="#."> SEO Optimization</a></li>
-                            <li><a href="#."> Web Analytics</a></li>
-                            <li><a href="#."> Digital Marketing</a></li>
-                        </ul>
-                    </div>
-
-                    <!-- useful links -->
-                    <div class="col-md-3">
-                        <h5>useful links</h5>
-                        <ul class="links">
-                            <li><a href="#.">About us</a></li>
-                            <li><a href="#."> Why Choose Us</a></li>
-                            <li><a href="#."> Our Services</a></li>
-                            <li><a href="#."> Contact Us</a></li>
-                            <li><a href="#."> Site Map</a></li>
-                            <li><a href="#."> Partners</a></li>
-                            <li><a href="#."> Help Topics</a></li>
-                        </ul>
-                    </div>
-
-                    <!-- flickr -->
-                    <div class="col-md-3">
-                        <h5>flickr</h5>
-                        <ul class="flicker">
-                            <li><a href="#."><img class="img-responsive" src="images/flicker-1.jpg" alt="" ></a></li>
-                            <li><a href="#."><img class="img-responsive" src="images/flicker-2.jpg" alt="" ></a></li>
-                            <li><a href="#."><img class="img-responsive" src="images/flicker-3.jpg" alt="" ></a></li>
-                            <li><a href="#."><img class="img-responsive" src="images/flicker-4.jpg" alt="" ></a></li>
-                            <li><a href="#."><img class="img-responsive" src="images/flicker-5.jpg" alt="" ></a></li>
-                            <li><a href="#."><img class="img-responsive" src="images/flicker-6.jpg" alt="" ></a></li>
-                        </ul>
-                    </div>
-                </div>
-
-                <!-- Links -->
-                <ul class="bottom-links">
-                    <li><a href="#.">Home </a></li>
-                    <li><a href="#."> about us </a></li>
-                    <li><a href="#."> services </a></li>
-                    <li><a href="#."> Portfolio</a></li>
-                    <li><a href="#."> blog </a></li>
-                    <li><a href="#."> contact us</a></li>
-                </ul>
-
-                <!-- Rights -->
-                <div class="rights">
-                    <p>© 2015 All Rights Reserved</p>
-                </div>
-            </div>
-        </footer>
-        <!-- End Footer -->
-    </div>
-    <!-- End Page Wrapper -->
+</div>
+</div>
+        <?php
+        include "footer.php";
+        ?>
 
     <!-- JavaScripts -->
     <script src="js/vendors/jquery/jquery.min.js"></script>
---
+++ public/maps.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<div id="map" style="width:100%;height:500px"></div>
+
+<script>
+    function myMap() {
+        var myLatLng = {lat: -7.3165519, lng: 112.7260529};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 18,
+            center: myLatLng
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: 'Jancuk'
+        });
+    }
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCVfdVQqnx9wPueW_jzovNd4VusGeSCax0&callback=myMap"></script>
+</body>
+</html>
