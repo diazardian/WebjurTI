207edf7f0d7b507cdfb0c89098e88bc162fc7198
Unknown <ddaiazardian@gmail.com>
2017-10-14 17:22:04 +0700

    edit index footer dan tambah footer.php

--- public/index.php
+++ public/index.php
@@ -685,87 +685,10 @@ include "header.php";
                 </div>
             </div>
         </section>
-
-
-        <!-- Footer -->
-
-        <footer>
-            <div class="container">
-                <div class="row">
-
-                    <!-- About -->
-                    <div class="col-md-3"> <img class="margin-bottom-20" src="images/logo-footer.png" alt="" >
-                        <p>Segelintir mahasiswa dari jurusan teknik informatika fakultas teknik Universitas Negeri Surabaya.</p>
-
-                        <!-- Social Icon -->
-                        <ul class="social-icons">
-                            <li><a href="#."><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="#."><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="#."><i class="fa fa-google"></i></a></li>
-                            <li><a href="#."><i class="fa fa-linkedin"></i></a></li>
-                            <li><a href="#."><i class="fa fa-dribbble"></i></a></li>
-                        </ul>
-                    </div>
-
-                    <!-- Our Services -->
-                    <div class="col-md-3">
-                        <h5>Kontak</h5>
-                        <ul class="links">
-                            <li><a href="#.">Wed Design</a></li>
-                            <li><a href="#."> Wed Development</a></li>
-                            <li><a href="#."> SEO Optimization</a></li>
-                            <li><a href="#."> Web Analytics</a></li>
-                            <li><a href="#."> Digital Marketing</a></li>
-                        </ul>
-                    </div>
-
-                    <!-- useful links -->
-                    <div class="col-md-3">
-                        <h5>pages</h5>
-                        <ul class="links">
-                            <li><a href="#."> Profil Jurusan</a></li>
-                            <li><a href="#."> Prtal Berita</a></li>
-                            <li><a href="#."> Akademik</a></li>
-                            <li><a href="#."> Sarana & Prasarana</a></li>
-                            <li><a href="#."> Dosen & Karyawan</a></li>
-                            <li><a href="#."> Kemahasiswaan</a></li>
-                            <li><a href="#."> Galeri</a></li>
-                        </ul>
-                    </div>
-
-                    <!-- flickr -->
-                    <div class="col-md-3">
-                        <h5>flickr</h5>
-                        <ul class="flicker">
-                            <li><a href="#."><img class="img-responsive" src="images/flicker-1.jpg" alt="" ></a></li>
-                            <li><a href="#."><img class="img-responsive" src="images/flicker-2.jpg" alt="" ></a></li>
-                            <li><a href="#."><img class="img-responsive" src="images/flicker-3.jpg" alt="" ></a></li>
-                            <li><a href="#."><img class="img-responsive" src="images/flicker-4.jpg" alt="" ></a></li>
-                            <li><a href="#."><img class="img-responsive" src="images/flicker-5.jpg" alt="" ></a></li>
-                            <li><a href="#."><img class="img-responsive" src="images/flicker-6.jpg" alt="" ></a></li>
-                        </ul>
-                    </div>
-                </div>
-
-                <!-- Links -->
-                <ul class="bottom-links">
-                    <li><a href="#."> Profil Jurusan</a></li>
-                    <li><a href="#."> Prtal Berita</a></li>
-                    <li><a href="#."> Akademik</a></li>
-                    <li><a href="#."> Sarana & Prasarana</a></li>
-                    <li><a href="#."> Dosen & Karyawan</a></li>
-                    <li><a href="#."> Kemahasiswaan</a></li>
-                    <li><a href="#."> Galeri</a></li>
-                </ul>
-
-                <!-- Rights -->
-                <div class="rights">
-                    <p>© 2017 All Rights Reserved</p>
-                </div>
-            </div>
-        </footer>
-        <!-- End Footer -->
     </div>
+<?php
+include "footer.php";
+?>
     <!-- End Page Wrapper -->
 
     <!-- JavaScripts -->
